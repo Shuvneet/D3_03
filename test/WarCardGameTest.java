87799a704f2e75f184680ab3f9c18800cee7713d
Shuvneet <kaurshuv@sheridancollege.ca>
2019-08-06 15:36:33 -0400

    I am saving PickACard program.

---
+++ test/WarCardGameTest.java
@@ -0,0 +1,54 @@
+/*Shuvneet kaur 
+Gurjinder singh mahal
+riya sharma
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author DELLS
+ */
+public class WarCardGameTest {
+    
+    
+    public WarCardGameTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class WarCardGame.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        WarCardGame.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
